e993f706bb94f4475d76f8d9bf43531c39cd9821
Eoghan West <eowest@gmail.com>
2026-01-16 13:54:47 -0500

    rmed bloated images

--- assets/maps/sprites.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="sprites" tilewidth="16" tileheight="16" spacing="1" tilecount="648" columns="54">
- <image source="sprites/Spritesheet/roguelikeChar_transparent.png" width="918" height="203"/>
-</tileset>
